f14a3e2a8ca8b61834c4e2b95d903ce4347731ef
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-02-15 15:13:09 +0900

    Update ViewController.swift

--- parapara-ui-table-view/ViewController.swift
+++ parapara-ui-table-view/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var imageArray: [UIImage] = [
+        UIImage(named: "image1")!,
+        UIImage(named: "image2")!,
+        UIImage(named: "image3")!,
+        UIImage(named: "image4")!,
+        UIImage(named: "image5")!,
+        UIImage(named: "image6")!,
+        UIImage(named: "image7")!,
+        UIImage(named: "image8")!
+    ]
+    var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.transform = CGAffineTransform(scaleX: 1, y: -1)
+        view.addSubview(tableView)
+        
+        tableView.register(ImageTableViewCell.self, forCellReuseIdentifier: "cell")
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ImageTableViewCell
+        
+        cell.contentView.transform = CGAffineTransform(scaleX: 1, y: -1)
+        cell.customImageView.image = imageArray[indexPath.row]
+        
+        cell.frame.size.width = view.bounds.width / 2
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row > 0 {
+            if let previousCell = tableView.cellForRow(at: IndexPath(row: indexPath.row - 1, section: indexPath.section)) {
+                let cellHeight = cell.frame.height
+                
+                cell.frame.origin.y = previousCell.frame.maxY - 20
+                cell.frame.origin.x = previousCell.frame.minX - 20
+            }
+        }
+    }
+}
