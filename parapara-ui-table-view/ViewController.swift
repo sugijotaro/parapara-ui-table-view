350ef45518b08ce2f79364a22e301ddbf631a317
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-02-15 14:44:07 +0900

    Initial Commit

---
+++ parapara-ui-table-view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  parapara-ui-table-view
+//
+//  Created by JotaroSugiyama on 2023/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
